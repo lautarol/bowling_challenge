05b9c38312c04b920b7e5c777de9ab0e5dd3823c
lautarol <llarroucau@gmail.com>
2020-11-09 20:51:37 -0300

    Reading the input file

--- bowling.rb
+++ bowling.rb
@@ -12,6 +12,26 @@ class Score
 end
 
 class FileReader
+    def initialize(file)
+        @players= read_file(file)
+    end
+
+    def read_file(file)
+        players=[]
+        players_shots = []
+        shots= File.read(file).split("\n")
+        names = []
+        shots.each do |shot|
+            names << shot.split(' ').first
+            players_shots << shot.split(' ')
+        end
+        names.uniq!
+        names.each do |player|
+            ps = players_shots.select{|p| p.first == player}.flatten!.reject{|p| p == player}
+            players << { name: player, shots: ps }
+        end
+        return players
+    end
 end
 
 class ResultWritter
